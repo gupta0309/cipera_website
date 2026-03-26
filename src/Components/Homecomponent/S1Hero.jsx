09dde1884d010db47e53de70ea513b845be97c15
sakshigupta0309 <sg524396@gmail.com>
2026-03-26 11:10:03 +0530

    cge7ergf87

--- src/Components/Homecomponent/S1Hero.jsx
+++ src/Components/Homecomponent/S1Hero.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import '../Styles/S1Hero.css';
 import video from "../../assets/bg-video.mp4"
+import coin1 from "../../assets/HomePage/coin1.png"
+import coin2 from "../../assets/HomePage/coin2.png"
+import coin3 from "../../assets/HomePage/coin4.png"
+import coin4 from "../../assets/HomePage/coin3.png"
 const Hero = () => {
   return (
     <section className="cpr-hero-section">
+<div className="cpr-coins">
       <video className="cpr-video-bg" autoPlay loop muted playsInline>
         <source src={video} type="video/mp4" />
       </video>
 
-      <div className="cpr-hero-radial-overlay"></div>
-
+      <div className="cpr-hero-radial-overlay">
+  </div>
+  <img src={coin1} className="coin coin-tl" alt="" />
+  <img src={coin2} className="coin coin-bl" alt="" />
+  <img src={coin3} className="coin coin-tr" alt="" />
+  <img src={coin4} className="coin coin-br" alt="" />
+</div>
       <div className="cpr-hero-content">
         <div className="cpr-badge-container">
           <span className="cpr-badge-text">CIPERA TOKEN</span>
